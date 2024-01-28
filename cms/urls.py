be7775f8814237ddffa7dd984edee2f427dec1a8
Ryan <vellabonelloryan@gmail.com>
2024-01-28 22:30:41 +0700

    feat: `index` & `show` pages for Post

--- cms/urls.py
+++ cms/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "blog"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.post_index, name="post_index"),
+    path("<int:id>", views.post_show, name="post_show"),
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Post
 
-def index(request):
-    return HttpResponse("This is my Blog!")
+
+def post_index(request):
+    posts = Post.objects.order_by('-created_at')[:12]
+    return render(request, 'posts/index.html', {'posts': posts})
+
+def post_show(request, id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request, 'posts/show.html', {'post': post})
